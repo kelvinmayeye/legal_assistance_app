71c89bc37c7b363871dfcd103e17a7168c6f8b1d
kelvinmayeye <kevmayeye97@gmail.com>
2023-04-22 15:39:10 +0300

    working on login screen

--- app/src/main/java/com/example/legalassistance/LoginActivity.java
+++ app/src/main/java/com/example/legalassistance/LoginActivity.java
@@ -1,10 +1,16 @@
 package com.example.legalassistance;
 
+import static android.app.ProgressDialog.show;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     EditText etEmail,etPassword;
@@ -21,5 +27,42 @@ public class LoginActivity extends AppCompatActivity {
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
         btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkLogin();
+            }
+        });
     }
+
+    private void checkLogin() {
+        email = etEmail.getText().toString();
+        password = etPassword.getText().toString();
+        if (email.isEmpty() || password.isEmpty()){
+            alertFail("Email and Password is required");
+        }
+        else {
+            sendLogin();
+        }
+    }
+
+    private void sendLogin() {
+        Toast.makeText(this,"send", Toast.LENGTH_LONG).show();
+    }
+
+    private void alertFail(String s) {
+        new AlertDialog.Builder(this)
+                .setTitle("Failed")
+                .setIcon(R.drawable.ic_warning)
+                .setMessage(s)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
 }
